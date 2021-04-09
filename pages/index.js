afc9f85d246d92f4717c903330cb0a67af64a701
Sharon-07 <sharonhernandez07@gmail.com>
2021-04-09 08:56:29 -0600

    matrices ingresadas

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
         </h1>
 
         <div className={styles.container}>
+          <h1>Matrices del apartado Códigos Binarios de Hamming</h1>
           <h3>Matriz 1</h3>
           <p> 
 &#8726;begin&#x2774;center&#x2775;
@@ -69,6 +70,77 @@ export default function Home() {
             <br />$
             <br />\end&#x2774;center&#x2775;       
           </p>
+
+          <h1>Matrices del apartado de Decodificación</h1>
+          <p>
+          <h3>Matriz 1</h3>
+          \begin&#x2774;center&#x2775;
+          <br />$
+          <br />H=
+          <br />\left(\begin&#x2774;array&#x2775;&#x2774;cccc&#x2775;
+            <br />   1 & 0 & 1 & 2 \\
+            <br />  0 & 1 & 1 & 1 
+            <br />\end&#x2774;array&#x2775; \right)
+            <br />$
+            <br />\end&#x2774;center&#x2775;
+          </p>
+          <p>
+          <h3>Matriz 2</h3>
+          \begin&#x2774;center&#x2775;
+          <br />$
+          <br />\left(\begin&#x2774;array&#x2775;&#x2774;cccc&#x2775;
+            <br />  1 & 0 & 1 & 2 \\
+            <br />  0 & 1 & 1 & 1
+            <br />\end&#x2774;array&#x2775; \right)
+            <br />$
+            <br />$
+            <br />\left(\begin&#x2774;array&#x2775;&#x2774;c&#x2775;
+              <br />   1 \\
+              <br />  2 \\
+              <br />  1 \\
+              <br />  2
+              <br />\end&#x2774;array&#x2775; \right)
+              <br />$
+              <br />=
+              <br />$
+              <br />\left(\begin&#x2774;array&#x2775;&#x2774;c&#x2775;
+                <br />   0 \\
+                <br />   2
+                <br />\end&#x2774;array&#x2775; \right)
+                <br />$
+                <br />=
+                <br />$
+                <br />2
+                <br />\left(\begin&#x2774;array&#x2775;&#x2774;c&#x2775;
+                  <br />   0 \\
+                  <br />   1
+                  <br />\end&#x2774;array&#x2775; \right)
+                  <br />$
+                  <br />\end&#x2774;center&#x2775;
+          </p>
+          <h1>Matrices del apartado de Códigos de Golay Binariosy Ternarios</h1>
+          <p>
+          \begin&#x2774;center&#x2775;
+          <br />$
+          <br />G<sub>24</sub>=
+          <br />\left(\begin&#x2774;array&#x2775;&#x2774;cccccccccccc&#x2775;
+            <br />   0 & 1 & 1 & 1 & 1 & 1 & 1 & 1 & 1 & 1 & 1 & 1 \\
+            <br />  1 & 1 & 1 & 0 & 1 & 1 & 1 & 0 & 0 & 0 & 1 & 0 \\
+            <br />  1 & 1 & 0 & 1 & 1 & 1 & 0 & 0 & 0 & 1 & 0 & 1 \\
+            <br />  1 & 0 & 1 & 1 & 1 & 0 & 0 & 0 & 1 & 0 & 1 & 1 \\
+            <br /> 1 & 1 & 1 & 1 & 0 & 0 & 0 & 1 & 0 & 1 & 1 & 0 \\
+            <br /> 1 & 1 & 1 & 0 & 0 & 0 & 1 & 0 & 1 & 1 & 0 & 1 \\
+            <br />  1 & 1 & 0 & 0 & 0 & 1 & 0 & 1 & 1 & 0 & 1 & 1 \\
+            <br /> 1 & 0 & 0 & 0 & 1 & 0 & 1 & 1 & 0 & 1 & 1 & 1 \\
+            <br />   1 & 0 & 0 & 1 & 0 & 1 & 1 & 0 & 1 & 1 & 1 & 0 \\
+            <br />  1 & 0 & 1 & 0 & 1 & 1 & 0 & 1 & 1 & 1 & 0 & 0 \\
+            <br /> 1 & 1 & 0 & 1 & 1 & 0 & 1 & 1 & 1 & 0 & 0 & 0 \\
+            <br /> 1 & 0 & 1 & 1 & 0 & 1 & 1 & 1 & 0 & 0 & 0 & 1 \\
+            <br />\end&#x2774;array&#x2775; \right)
+            <br />$
+            <br />\end&#x2774;center&#x2775;
+          </p>
+ 
         </div>
       </main>
 
